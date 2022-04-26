bd60a34fa4d926a86628c397edd9e1bbe3a78e4a
SomeBrujah <wissenseeker@gmail.com>
2022-04-26 15:54:18 +0300

    Remove circular structure

--- server/index.js
+++ server/index.js
@@ -1,26 +1,28 @@
 const Koa = require('koa');
 const Router = require('koa-router');
-const path = require('path');
-const serve = require('koa-static');
-const { Game } = require('./test/game');
-const { Player } = require('./test/players');
+const { Game } = require('./app/game');
+const { Player } = require('./app/players');
 const app = new Koa();
 const router = new Router();
-let testCounter = 0;
 
 const game = new Game([new Player('Jesica'), new Player('Jack')]);
 game.start();
 
-app.use(serve(path.join(__dirname, 'public')));
-
 router.post('/hit', async (ctx) => {
     game.currentPlayer.hit();
+    ctx.body = game;
 });
 
 router.post('/stand', async (ctx) => {
     game.currentPlayer.stand();
+    ctx.body = game;
 })
 
+router.post('/restart', async (ctx) =>{
+    game = new Game([new Player('Jesica'), new Player('Jack')]);
+    game.start();
+    ctx.body = game;
+})
 
 app.use(router.routes());
 const PORT = process.env.PORT || 3000;
--- src/card.js
+++
@@ -1,7 +0,0 @@
-export class Card {
-    constructor(value, suit, scores){
-        this.value = value;
-        this.suit = suit;
-        this.scores = scores;
-    }
-}
